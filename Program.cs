70f9e3546eb9c88e7c268710ff01cbce0179d41d
Your Name <you@example.com>
2022-10-20 15:58:06 +0300

    Add Readme

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir kelime giriniz ve ardından çıkarmak istediğiniz karakterin indeksini giriniz(Aralarında virgül kullanınız):");
+            string word = Console.ReadLine();
+
+            string[] word1 = word.Split(","); 
+            string kelime = word1[0];
+            int çevirme = Convert.ToInt32(word1[1]); 
+            string sonHal = kelime.Remove(çevirme, 1); 
+
+            Console.WriteLine(sonHal);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# algoritma
+Ekrandan bir string bir de sayı alan (aralarında virgül ile), ilgili string ifade içerisinden verilen indexteki karakteri çıkartıp ekrana yazdıran console uygulaması
